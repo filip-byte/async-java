bcfae9f0f37b972ebb96b018867c12de2cd4655a
filip-byte <ohboytoby@gmail.com>
2024-08-12 11:28:55 +0100

    Task 1: completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,26 @@
+import java.util.concurrent.CompletableFuture;
+
 public class Main {
-    int var = 1;
+
+    public static void main(String[] args) {
+
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+            try {
+            Thread.sleep(5000);
+            System.out.println("Hello World!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        });
+
+        future.join();
+
+    }
+
+
+
 }
+
+
+
+
