aafb129df47e4c19ba94a77a1e07cb2a85146720
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-08-12 15:49:55 +0100

    feat: task 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,31 @@
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import javax.naming.TimeLimitExceededException;
+import java.util.Random;
+import java.util.concurrent.*;
 import java.util.function.BiFunction;
 
 public class Main {
 
     //test
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TimeoutException {
+
+        Random random = new Random();
+        var test = random.nextInt(1, 20);
+//        System.out.println(test);
 
         CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
             try {
-                Thread.sleep(3000);
+                Thread.sleep(test * 1000L);
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
             return "Hello";
         });
+
         CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
             try {
-                Thread.sleep(3000);
+                Thread.sleep(test * 1000L);
+//                future1.orTimeout(test, TimeUnit.MILLISECONDS);
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
@@ -26,10 +33,11 @@ public class Main {
         });
 
 
-        CompletableFuture<String> combinedFuture = future1.thenCombine(future2, (result1, result2) -> result1 + " " + result2);
-        combinedFuture.thenAccept(result -> System.out.println("Combined result: " + result));
 
-        combinedFuture.join();
+
+        CompletableFuture<String> combinedFuture = future1.thenCombine(future2, (result1, result2) -> result1 + " " + result2);
+        combinedFuture.thenAccept(result -> System.out.println("Combined result: " + result)).orTimeout(10, TimeUnit.SECONDS).join();
+
 
 
     }
